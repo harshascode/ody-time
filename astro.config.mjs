aeeb2684e201451035efb2ad58a118b188072ff3
Harsha <118063362+harshascode@users.noreply.github.com>
2024-03-23 07:39:04 -0700

    updatge

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.sapling.supply/',
-  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  site: 'https://ody-time.vercel.app/',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs. main page: https://odyssey-theme.sapling.supply/
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon(), tailwind()] // Add renderers to the config
